163456293e875a3fd9722af3490db62e255053df
Mars <marce.crfx@gmail.com>
2019-04-28 15:00:24 -0500

    First random test
    Figuring out how to use this thing ._.

--- index.js
+++ index.js
@@ -1,46 +1,63 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-const slack = require('slack');
-const moment = require('moment');
+var myArray = [
+  "nope",
+  "nope",
+  "nope",
+  "verduras",
+  "carbs",
+  "verduras",
+  "frutas",
+  "carne",
+  "semillas"
+];
 
-const app = express();
-const port = process.env.PORT || 5000;
+var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
 
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+document.body.innerHTML = randomItem;
 
-const router = express.Router();
 
-app.post('/', (req, res, next) => {
-  // check for secret token
-  if (!req.body.token || req.body.token !== process.env.SECRET_TOKEN) next();
-  // grab status and clean it up
-  let status = req.body.title;
-  const dndToken = ' [DND]';
-  // parse event start/stop time
-  const dateFormat = 'MMM D, YYYY [at] hh:mmA';
-  const start = moment(req.body.start, dateFormat);
-  const end = moment(req.body.end, dateFormat);
-  // check for DND
-  if (status.includes(dndToken)) {
-    slack.dnd.setSnooze({
-      token: process.env.SLACK_TOKEN,
-      num_minutes: end.diff(start, 'minutes')
-    });
-    status = status.replace(dndToken, '');
-  }
-  // set status
-  slack.users.profile.set({
-    token: process.env.SLACK_TOKEN,
-    profile: JSON.stringify({
-      "status_text": `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`
-    })
-  });
-  res.status(200);
-  res.send('🤘');
-});
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const logger = require('morgan');
+// const slack = require('slack');
+// const moment = require('moment');
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(logger('dev'));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+// const router = express.Router();
+
+// app.post('/', (req, res, next) => {
+//   // check for secret token
+//   if (!req.body.token || req.body.token !== process.env.SECRET_TOKEN) next();
+//   // grab status and clean it up
+//   let status = req.body.title;
+//   const dndToken = ' [DND]';
+//   // parse event start/stop time
+//   const dateFormat = 'MMM D, YYYY [at] hh:mmA';
+//   const start = moment(req.body.start, dateFormat);
+//   const end = moment(req.body.end, dateFormat);
+//   // check for DND
+//   if (status.includes(dndToken)) {
+//     slack.dnd.setSnooze({
+//       token: process.env.SLACK_TOKEN,
+//       num_minutes: end.diff(start, 'minutes')
+//     });
+//     status = status.replace(dndToken, '');
+//   }
+//   // set status
+//   slack.users.profile.set({
+//     token: process.env.SLACK_TOKEN,
+//     profile: JSON.stringify({
+//       "status_text": `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`
+//     })
+//   });
+//   res.status(200);
+//   res.send('🤘');
+// });
 
 app.get('/', (req, res, next) => {
   // welcome message
@@ -64,9 +81,8 @@ app.get('/', (req, res, next) => {
         <h3>Body</h3>
 <pre>{
   "title":"<<<{{Title}}>>>",
-  "start":"{{Starts}}",
+  "start":"{{andomItem}}",
   "end":"{{Ends}}",
-  "token": "${process.env.SECRET_TOKEN}"
 }</pre>
       </body>
     </html>
